22b3ded2d26fe9c9cc977f1f93306a2822de4fc5
xdarov <xdarovv@gmail.com>
2022-06-27 20:35:39 +0300

    logickWork

--- My project/Assets/Scripts/ObjektMove.cs
+++ My project/Assets/Scripts/ObjektMove.cs
@@ -1,34 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ObjektMove : MonoBehaviour
 {
-    private bool mouse1 = false;
-    private Rigidbody rb;
+    private bool compliteLvl = false;
+    public Vector3 correct_position;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    private Vector3 move_x = new Vector3(.1f, 0f, 0f);
+    private Vector3 move_y = new Vector3(0f, .1f, 0f);
+    private Vector3 move_z = new Vector3(0f, 0f, .1f);
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && !compliteLvl)
         {
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
             transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y") * 20, Space.World);
             transform.Rotate(Vector3.down, Input.GetAxis("Mouse X") * 20, Space.World);
         }
-        // if (Input.GetKey("q"))
-        // {
-        //     rb.AddTorque(-Vector3.left, ForceMode.VelocityChange);
-        //     rb.AddTorque(-Vector3.up, ForceMode.VelocityChange);
-
-        // }
-    }
-    void FixedUpdate() 
-    {
+        if (Input.GetKeyDown("z"))
+        {
+            if (!compliteLvl)
+                compliteLvl = true;
+            else
+                compliteLvl = false;
+        }
+        if (!compliteLvl && Math.Abs(transform.eulerAngles[0] - correct_position[0]) < 30 && Math.Abs(transform.eulerAngles[1] - correct_position[1]) < 30 && Math.Abs(transform.eulerAngles[2] - correct_position[2]) < 30)
+        {
+            Debug.Log("You Win!");
+            compliteLvl = true;
+        }
+        if (compliteLvl)
+        {
+            if (transform.eulerAngles[0] < correct_position[0])
+                transform.eulerAngles += move_x;
+            else
+                transform.eulerAngles -= move_x;
+            if (transform.eulerAngles[1] < correct_position[1])
+                transform.eulerAngles += move_y;
+            else
+                transform.eulerAngles -= move_y;
+            if (transform.eulerAngles[2] < correct_position[2])
+                transform.eulerAngles += move_z;
+            else 
+                transform.eulerAngles -= move_z;
+        }
+        if (Input.GetKeyDown("i"))
+        {
+            Debug.Log(transform.eulerAngles + " " + correct_position);
+        }
     }
 }
+
+
