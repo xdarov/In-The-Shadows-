85b34f013767cc48bb8455db124a16a2b851fbaa
xdarov <xdarovv@gmail.com>
2022-06-27 13:44:09 +0300

    objMove.v1

--- My project/Assets/Scripts/ObjektMove.cs
+++ My project/Assets/Scripts/ObjektMove.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ObjektMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool mouse1 = false;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y") * 20, Space.World);
+            transform.Rotate(Vector3.down, Input.GetAxis("Mouse X") * 20, Space.World);
+        }
+        // if (Input.GetKey("q"))
+        // {
+        //     rb.AddTorque(-Vector3.left, ForceMode.VelocityChange);
+        //     rb.AddTorque(-Vector3.up, ForceMode.VelocityChange);
+
+        // }
+    }
+    void FixedUpdate() 
+    {
     }
 }
